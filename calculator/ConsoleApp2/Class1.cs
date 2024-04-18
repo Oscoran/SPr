dd4eb76c97a3ff379a5aa4891cb4ec01b4086d6d
Oscoran <96298611+Oscoran@users.noreply.github.com>
2024-04-18 11:33:03 +0200

    Add files via upload

---
+++ calculator/ConsoleApp2/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Calculator
+    {
+        public int Secti(int a, int b)
+        {
+            return a + b;
+        }
+        private int Diskriminant (int a, int b, int c)
+        {
+            return (b*b)-4*a*c;
+        }
+        public double[] ResKvardRci (int a, int b, int c)
+        {
+            double[] koreny = new double[2];
+            if (Diskriminant (a,b,c)> 0)
+            {
+                koreny[0] = -b + Math.Sqrt(Diskriminant(a, b, c)) / (2 * a);
+                koreny[1] = -b - Math.Sqrt(Diskriminant(a, b, c)) / (2 * a);
+                return koreny;
+            }
+            if (Diskriminant(a, b, c) == 0)
+            {
+                koreny[0] = -b/ (2 * a);
+                
+                return koreny;
+            }
+            else
+            {
+                koreny = null;
+                return koreny;
+            }
+            
+        }
+    }
+}
---
+++ calculator/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp2;
+// vytvorit instanci tridy kalkulacka
+Calculator calculator = new Calculator();
+
+int a = 4;
+int b = 1;
+int c = 3;
+Console.WriteLine("the roots are:");
+foreach (double item in calculator.ResKvardRci(a,b,c))
+{
+	
+    
+    
+        Console.WriteLine(item);
+    
+    
+}
+Console.ReadKey();
