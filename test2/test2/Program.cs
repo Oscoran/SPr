a67de5968dcda72afb70356df37bb89fd32d6842
Oscoran <96298611+Oscoran@users.noreply.github.com>
2024-04-25 11:08:05 +0200

    Add files via upload

---
+++ test2/test2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+StreamReader sr = new StreamReader("zadani.txt");
+string input = sr.ReadLine();
+string nej = input;
+int pocet = input.Length;
+int delka = 0;
+int cholik = 0;
+List <string> pole = new List<string>();
+while (input != null)
+{
+	delka++;
+	if (pocet < input.Length)
+	{
+		nej = input;
+		pocet = input.Length;
+
+	}
+	if (input.Contains("ch"))
+	{
+		cholik++;
+	}
+	if (input.Contains('z'))
+	{
+		input = input.Replace('z', 'ř') ;
+	}
+    pole.Add(input);
+    input = sr.ReadLine();
+}
+foreach (string p in pole) { Console.WriteLine(	p); }
+Console.WriteLine(delka);
+Console.WriteLine(nej	);
+Console.WriteLine(cholik);
