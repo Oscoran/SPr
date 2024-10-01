3abd288bbd0caf50c5b84ebab0556216e7db1edf
Oscoran <96298611+Oscoran@users.noreply.github.com>
2024-10-01 15:50:06 +0200

    Add files via upload

---
+++ wannabeduolingo/wannabeduolingo/Data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wannabeduolingo
+{
+    internal class Data
+    {
+        private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        Random rnd = new Random();
+        List<string> elements;
+        List<string> lkey;
+        public Data()
+        {
+            using StreamReader sr = new StreamReader(@"pojmy.txt");
+            string pivo;
+            string[] splitstream = new string[2];
+            while ((pivo = sr.ReadLine()) != null)
+            {
+                splitstream = pivo.Split("*");
+                dictionary.Add(splitstream[0], splitstream[1]);
+            }
+            update();
+        }
+       
+
+        public void addthing(string key, string date)
+        {
+            using StreamWriter sw = new StreamWriter(@"pojmy.txt", true);
+            {
+                sw.WriteLine($"{key}*{date}");
+                dictionary.Add(key, date);
+                update();
+            }
+        }
+        public void update()
+        {
+            elements = new(dictionary.Values);
+            lkey = new(dictionary.Keys);
+        }
+        public string giveKey()
+        {
+            List<string> lkey = new(dictionary.Keys);
+            return lkey[rnd.Next(dictionary.Count)];
+        }
+        public string giveValue(string key)
+        {
+            
+            return dictionary[key];
+        }
+    }
+}
---
+++ wannabeduolingo/wannabeduolingo/Program.cs
@@ -0,0 +1,74 @@
+﻿using wannabeduolingo;
+while (true)
+{
+    Data data = new Data();
+    Console.WriteLine("zkouseni z dat");
+    Console.WriteLine("vyber:");
+    Console.WriteLine("1: pridej pojem");
+    Console.WriteLine("2: zkouseni");
+    int volba = int.Parse(Console.ReadLine());
+    bool bol = true;
+    switch (volba)
+    {
+        case 1: //pridani pojmu
+            string key = "";
+            string value;
+            Console.WriteLine("zadavej pojmy,pro ukonceni napis 1");
+
+            while (key != "1")
+            {
+                Console.WriteLine("udalost:");
+                key = Console.ReadLine();
+                if (key != "1")
+                {
+                    Console.WriteLine("Datum: ");
+                    value = Console.ReadLine();
+                    data.addthing(key, value);
+                }
+
+            }
+            break;
+        case 2: //zkouseni
+            while (bol== true)
+            {
+                Console.WriteLine("odpovidej datum nebo rok, nebo co chces co ja vim");
+                Console.WriteLine("kolik chces otazek?");
+                int lop = int.Parse(Console.ReadLine());
+                string answer;
+                int wrong = 0;
+                int correct = 0;
+                for (int i = 0; i < lop; i++)
+                {
+                    key = data.giveKey();
+                    Console.WriteLine(key);
+
+                    answer = Console.ReadLine();
+                    if (answer == data.giveValue(key))
+                    {
+                        Console.WriteLine("ANO");
+                        correct++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("blby");
+                        wrong++;
+                    }
+
+                }
+                Console.WriteLine($"spravne: {correct}, spatne {wrong}");
+                Console.WriteLine("by stacilo ne? (ANO/NE)");
+                answer = Console.ReadLine();
+                if (answer == "NE")
+                {
+                    bol = true ;
+                }
+                if (answer == "ANO")
+                {
+                    bol = false ;
+                }
+            }
+            
+            
+            break;
+    }
+}
